d3e250c9ed98f1a4d20295518605f698e30922cb
denizatlihan <denizatlihan2208@gmail.com>
2023-09-03 16:02:31 +0300

    nextconfig fs false

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const { config } = require('process')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     serverRuntimeConfig:{
         PROJECT_ROOT: __dirname
+    },
+    webpack: (config,{isServer}) => {
+        if(!isServer){
+            config.node = {
+                fs: 'empty'
+            }
+        }
     }
 }
 
