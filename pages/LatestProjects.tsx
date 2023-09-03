deadd2e2dbf898b0182e20303ee4990a43d52a08
denizatlihan <denizatlihan2208@gmail.com>
2023-09-03 16:59:11 +0300

    staticprobe

--- pages/LatestProjects.tsx
+++ pages/LatestProjects.tsx
@@ -2,11 +2,31 @@ import fs from 'fs'
 import { join } from 'path'
 import getConfig from 'next/config'
 
+export function getStaticProps() {
+
+    const projectsDir = join(process.cwd(), "projects");
+    const files = fs.readdirSync(projectsDir);
+
+    // let projects = files.map((filename) => {
+    //     const slug = filename.replace(/\.md$/, '')
+    //     // const fullpath = join(projectsDir,'${slug}.md')
+    //     // const readFile = fs.readFileSync(fullpath,'utf8')
+    //     // const {data: frontmatter} = matter(readFile)}
+    //     return { slug };
+    // }
+    // )
+
+    return {
+        props: {
+            files
+        }
+    }
+}
+
 export default function LatestProjects() {
 
-    const dir = join(process.cwd(),'projects')
-    const slugs=  fs.readdirSync(dir)
-    
-    return (<ul>{slugs.map(slug=><li>{slug}</li>)}</ul>)
+    const sp = getStaticProps()
+
+    return (<ul>{<li>{sp.props.files}</li>}</ul>)
 
 }
