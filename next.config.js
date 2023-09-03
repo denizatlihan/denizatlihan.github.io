3aff12d8432dc721f987b720231bc82e7e431247
denizatlihan <denizatlihan2208@gmail.com>
2023-09-03 16:05:56 +0300

    Revert " nextconfig fs false"
    This reverts commit d3e250c9ed98f1a4d20295518605f698e30922cb.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-const { config } = require('process')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     serverRuntimeConfig:{
         PROJECT_ROOT: __dirname
-    },
-    webpack: (config,{isServer}) => {
-        if(!isServer){
-            config.node = {
-                fs: 'empty'
-            }
-        }
     }
 }
 
